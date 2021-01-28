8dca891049b679f78c8cd56c1e6222ef0a159398
Edward Regalado <tregalado30@gmail.com>
2021-01-27 21:36:23 -0800

    worked on functions for Seattle

--- js/app.js
+++ js/app.js
@@ -1,16 +1,53 @@
 'use strict';
 
-console.log('Hello World');
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let seattle = {
   name: 'Seattle',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-}
+  // the min number of customers per hour.
+  minimunCustomersEachHour: 23,
+  // the max number of customers per hour.
+  maximumCustomerEachHour: 65,
+  // the average number of cookies purchased per customer.
+  avgerageCookieSoldPerCustomer: 6.3,
 
+  cookiesSoldPerHourArray: [],
+  // swill hold the calculated number of cookies sold in the store all day long
+
+  dailyStoreTotal: 0,
+  // a metho to calculate random number of customers per hour
+  randomCustomersEachHour: function () {
+    //console.log('Im in randomCustomerEachHour');
+    // generates a random number of customers inclusive of the min and the max
+    return Math.floor(Math.randon() * (this.maximumCustomerEachHour - this.minimunCustomersEachHour + 1) + this.minimunCustomersEachHour);
+  }
+
+  // a method to calculate and populate our number of cookies sold per hour array
+  calcCookiesSoldEachHour: function () {
+    for (var i = 0; i < hours.length; i++) {
+      let cookiesSold = Math.ceil(this.randomCustomerEachHour() / this.avgerageCookieSoldPerCustomer);
+      this.cookiesSoldPerHourArray.push(cookiesSold);
+      console.log(this.cookiesSoldEachHour);
+      }
+    }
+    //console.log(randomCustomerEachHour);
+
+    // do something maybe use a for loop
+    // multiply customer number by average
+    // handle the number do some rounding
+    // push into the cookieSoldPerHourArray 
+  }
+
+  // a method to render the list items
+  render: function () {
+    this.calcCookiesSoldEachHour();
+    console.log('I an in the render method');
+    // do something
+  }
+};
+
+seattle.render();
 
 
 let tokyo = {
